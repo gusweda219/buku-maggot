65a69fdee3d379f3e4c796ffcc447a85c5306bb1
gusweda219 <wedabaskara219@gmail.com>
2021-10-11 20:40:44 +0800

    add page

---
+++ lib/ui/myfarm_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyFarmPage extends StatelessWidget {
+  const MyFarmPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('MyFarm Page'),
+      ),
+    );
+  }
+}
